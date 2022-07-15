49575e84896771b671e21ced08973ff7ff9fc11f
Fernanda <fernandaos12@gmail.com>
2022-07-15 18:28:42 -0300

    Crud com EF

--- MinApi/Program.cs
+++ MinApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 //habilitar o swagger como serviço
@@ -5,6 +8,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //adicionando serviço usando tempo de vida AddSingleton incluindo testeService como dependencia na colecao de servicos
 builder.Services.AddSingleton<TesteService>(new TesteService());
+
+//Adicionando DBContext para EF depois de adicionar o package Microsoft.EntityFrameworkCore.InMemory
+builder.Services.AddDbContext<AppDbContext>(options=>
+   options.UseInMemoryDatabase("Livros"));
+
 
 var app = builder.Build();
 
@@ -27,6 +35,40 @@ app.MapGet("/catalogo", () => new Catalogo("livro01", "autor01"));
 app.MapGet("/album", async () =>
     await new HttpClient().GetStringAsync("https://jsonplaceholder.typicode.com/albums"));
 
+//consumindo de um EF
+app.MapGet("/Livros/{id}", async (int id, AppDbContext dbContext) =>
+            {
+               await dbContext.Livros.FirstOrDefaultAsync(a=> a.Id == id);
+            });
+
+app.MapPost("/Livros/", async (Livro livro, AppDbContext dbContext) =>
+    {
+        dbContext.Livros.Add(livro);
+        await dbContext.SaveChangesAsync();
+        return livro;
+    }
+);
+
+app.MapPut("/Livros/{id}", async(int id, Livro livro, AppDbContext dbContext) =>
+    {
+        dbContext.Entry(livro).State = EntityState.Modified;
+        await dbContext.SaveChangesAsync();
+        return livro;
+    }
+);
+
+app.MapDelete("/Cliente/{id}", async (int id, AppDbContext dbContext) =>
+    {
+        var livro = await dbContext.Livros.FirstOrDefaultAsync(a=> a.Id == id);
+        if(livro != null)
+        {
+            dbContext.Livros.Remove(livro);
+            await dbContext.SaveChangesAsync();
+        }
+        return;
+    });
+
+
 app.UseSwaggerUI();
 
 await app.RunAsync();
@@ -39,4 +81,21 @@ public class TesteService
     {
         return $"Bem Vindo {nome}";
     }
+}
+
+
+public class Livro{
+    public int Id{get; set;}
+    public string? Nome { get; set; }    
+    
+}
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options): base(options)
+    {
+
+    }
+
+    public DbSet<Livro>? Livros{get;set;}
 }
