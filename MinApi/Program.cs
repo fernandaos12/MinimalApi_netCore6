57b36ea4d72e241f8d0a79f48627eefc952466d4
Fernanda <fernandaos12@gmail.com>
2022-07-31 13:46:28 -0300

    Ajustes Program.cs Endpoints

--- MinApi/Program.cs
+++ MinApi/Program.cs
@@ -1,53 +1,24 @@
 using Microsoft.EntityFrameworkCore;
-
+using MinApi.Endpoints;
+using MinApi.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//habilitar o swagger como serviço
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.AddPersistence();
+
 var app = builder.Build();
+
+app.MapLivroEndpoints();
 
 app.UseSwagger();
 
 
 app.UseSwaggerUI();
 
-await app.RunAsync();
+app.UseHttpsRedirection();
 
-//adicionando serviço usando tempo de vida AddSingleton incluindo testeService como dependencia na colecao de servicos
-builder.Services.AddSingleton<TesteService>(new TesteService());
-
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
-
-
-
-public class TesteService
-{
-    public string BoasVindas(string nome)
-    {
-        return $"Bem Vindo {nome}";
-    }
-}
-
-
-public class Livro{
-    public int Id{get; set;}
-    public string? Nome { get; set; }    
-    
-}
-
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions options): base(options)
-    {
-
-    }
-
-    public DbSet<Livro> Livros{get;set;}
-}
+app.Run();
